22b8b31189676742f11de3a6ba287e5b3534a72c
stashy <116462396+sstashy@users.noreply.github.com>
2023-08-02 20:04:43 +0300

    Update sekskolesi.php

--- sekskolesi.php
+++ sekskolesi.php
@@ -1,35 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>hacked by WS-Check</title>
-    <meta charset="utf-8">
-</head>
-<body bgcolor="black" style="color:dark">
-    <div style="align-items: center;">
-    <center><br><br><br>
-        <a target="_blank" href="https://discord.gg/wscheck" ><img src=""></a><br><br><br>
-        
-          
-        <font face="Courier New" color="red" size="5">
-            <span style="text-shadow: 0px 0px 9px ;">
-                <strong>hacked by stashy & w4ferz</strong></span>
-        </font><br><br><br><br>
-        <font face="Courier New" color="red" size="3"></font>
-        <font face="Courier New" color="white" size="4"><strong>AM YİYEN TAYFA 123</strong><br><br><br>
-            <font face="Courier New" color="white" size="4"><strong>zeks</strong><br><br><br>
-                <font face="Courier New" color="white" size="4"><strong>zeks</strong><br><br><br>
-            <font face="Courier New" color="red" size="5"><span style="text-shadow: 0px 0px 9px ;"><strong><a target="_blank" href="https://discord.gg/wscheck" style="color: red;">.gg/wscheck</a></strong></span></font><br><br><br>
-            <font face="Courier New" color="white" size="3">
-                <div name="divB" style="display: ;">
-                    <span style="text-shadow: 0px 0px 9px ;"><strong  ><a target="_blank" href="https://discord.gg/wscheck" style="color: white;">discord .gg/wscheck</a></strong></span></font>&nbsp;&nbsp;
-                </div>
-                <div style="align-items: center;">
-                <br>
+<html lang=”tr”><head>
 
-                </div>
-    </div>
-    </center>
-    </font>
-    </font>
+    <title>Hacked By Regex</title>
+    </head>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <body bgcolor="black" style="color:dark"><center>
+         <script>
+        body {
+    background-color: #000000;
+}
+       </script>
+    <br><br><br>
+    
+    <font face="Courier New" color="white" size="9"><span style="text-shadow: 0px 0px 9px ;"><strong>amyiyentayfa</strong></span></font>
+    
+    <br><br><hr width="9%"><br><br>
+    
+    <font face="Courier New" color="white" size="5"><span style="text-shadow: 0px 0px 9px ;"><strong>Hack</strong> bir <strong>zanaattir zanaat</strong> ise bir <strong>yetenektir! </strong></span></font>&nbsp;&nbsp;<font face="Courier New" color="red" size="5"></font>
+ <br><br>   
+    <font face="Courier New" color="white" size="6">- wsglobal.icu - <br> 
+    
+    <br><hr width="9%"><br>
+    
 
-</html>
+    </font></font></font></center></body></html>
