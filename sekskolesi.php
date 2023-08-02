9fb90c496d1eee96d1c98f9e947ebcefacfae176
stashy <116462396+sstashy@users.noreply.github.com>
2023-08-02 20:02:15 +0300

    Create sekskolesi.php

---
+++ sekskolesi.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>hacked by WS-Check</title>
+    <meta charset="utf-8">
+</head>
+<body bgcolor="black" style="color:dark">
+    <div style="align-items: center;">
+    <center><br><br><br>
+        <a target="_blank" href="https://discord.gg/wscheck" ><img src=""></a><br><br><br>
+        
+          
+        <font face="Courier New" color="red" size="5">
+            <span style="text-shadow: 0px 0px 9px ;">
+                <strong>hacked by stashy & w4ferz</strong></span>
+        </font><br><br><br><br>
+        <font face="Courier New" color="red" size="3"></font>
+        <font face="Courier New" color="white" size="4"><strong>AM YİYEN TAYFA 123</strong><br><br><br>
+            <font face="Courier New" color="white" size="4"><strong>zeks</strong><br><br><br>
+                <font face="Courier New" color="white" size="4"><strong>zeks</strong><br><br><br>
+            <font face="Courier New" color="red" size="5"><span style="text-shadow: 0px 0px 9px ;"><strong><a target="_blank" href="https://discord.gg/wscheck" style="color: red;">.gg/wscheck</a></strong></span></font><br><br><br>
+            <font face="Courier New" color="white" size="3">
+                <div name="divB" style="display: ;">
+                    <span style="text-shadow: 0px 0px 9px ;"><strong  ><a target="_blank" href="https://discord.gg/wscheck" style="color: white;">discord .gg/wscheck</a></strong></span></font>&nbsp;&nbsp;
+                </div>
+                <div style="align-items: center;">
+                <br>
+
+                </div>
+    </div>
+    </center>
+    </font>
+    </font>
+
+</html>
